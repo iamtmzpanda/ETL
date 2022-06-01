8253cc6b919c0a17ee36a83db4e4bcfc84d7183e
iamtmzpanda <105471179+iamtmzpanda@users.noreply.github.com>
2022-05-31 20:59:14 -0400

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,17 @@
+# SELECT
+
+
+
+
+
+# FROM
+-- LATERAL VIEW
+SELECT survey_id, question_id_string
+FROM dim_survey_definition LATERAL VIEW EXPLODE(question_ids_array) AS question_id_string
+
+
+
+
+
+
+# WHERE
