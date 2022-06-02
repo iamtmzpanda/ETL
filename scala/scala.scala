ec064985387c1699ae3c2cc2a8a865dff2e161dc
iamtmzpanda <105471179+iamtmzpanda@users.noreply.github.com>
2022-06-01 22:31:15 -0400

    Update scala.scala

--- scala/scala.scala
+++ scala/scala.scala
@@ -25,6 +25,10 @@ try {
 foreach iterates over a list and performs some operation with side effects to each list member (such as saving each one to the database for example)
 map iterates over a list, transforms each member of that list, and returns another list of the same size with the transformed members (such as converting a list of strings to uppercase)
 """
+val donuts4: Seq[String] = donuts3.map {
+ case donut: String => donut + " Donut"
+ case None => "Unknown Donut"
+}
 
 
 // reflection
