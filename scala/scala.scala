fed415e7d63525cf8b3fe98dfeefad9b75068946
iamtmzpanda <105471179+iamtmzpanda@users.noreply.github.com>
2022-06-01 22:22:11 -0400

    Create scala.scala

---
+++ scala/scala.scala
@@ -0,0 +1,32 @@
+// function
+val
+def
+
+// exception 
+try {
+    block
+} catch {
+    case pattern if guard => statements
+    case pattern => statements
+} finally {
+    block
+}
+
+// case class
+// pattern matching 
+
+
+// collection
+
+
+// map
+// foreach
+""" 
+foreach iterates over a list and performs some operation with side effects to each list member (such as saving each one to the database for example)
+map iterates over a list, transforms each member of that list, and returns another list of the same size with the transformed members (such as converting a list of strings to uppercase)
+"""
+
+
+// reflection
+
+// dependency injection
